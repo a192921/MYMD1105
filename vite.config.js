7988be9a02405415e4746544e98b2bf4eb596962
吳卿瑤 <queena@queenadeMacBook-Pro-2.local>
2026-01-21 20:34:10 +0800

    0121_1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // 或使用 true,允許外部訪問
+    port: 80, // 默認端口,可以自定義
+    strictPort: false, // 端口被佔用時自動嘗試下一個
+    open: true, // 啟動時自動打開瀏覽器
+  }
 })
